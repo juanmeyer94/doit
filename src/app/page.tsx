2f4cdff8c371bb56cdc1ac191b9383542bae2ef9
juanmeyer94 <juan_meyer@hotmail.com.ar>
2024-11-26 10:11:15 -0300

    little fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import styles from '@/styles/landing.module.css'
+import styles from '@/styles/Landing.module.css'
 
 export default function LandingPage() {
   return (
